167f91ae84e21d42c519f3ba4000ac97fdef371b
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-26 21:32:58 +0200

    feat: :sparkles: positional argument for reading files

--- main.c
+++ main.c
@@ -15,7 +15,31 @@
 #define COLOR_COUNT 6
 #define TYPE_SPEED 10
 
-void setup_colors(void) {
+char *read_file(char *file_path) {
+  FILE *file = fopen(file_path, "r");
+
+  if (!file)
+    return NULL;
+
+  fseek(file, 0, SEEK_END);
+  long file_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  char *buffer = malloc(sizeof(char) * (file_size + 1));
+  fread(buffer, sizeof(char), file_size, file);
+  buffer[file_size] = '\0';
+
+  fclose(file);
+
+  return buffer;
+}
+
+void setup_curses(void) {
+  initscr();
+
+  cbreak();
+  noecho();
+
   if (can_change_color()) {
     start_color();
     init_pair(1, COLOR_WHITE, COLOR_BLACK);
@@ -28,6 +52,23 @@ void setup_colors(void) {
   }
 }
 
+WINDOW *create_window(void) {
+  // get number of lines and cols
+  int lines, cols;
+  getmaxyx(stdscr, lines, cols);
+
+  WINDOW *win = newwin(lines - 2, cols - 2, 1, 1);
+  scrollok(win, TRUE);
+
+  return win;
+}
+
+void random_color(WINDOW *win) {
+  int color_index = rand() % COLOR_COUNT + 1;
+  wbkgd(stdscr, COLOR_PAIR(color_index));
+  wbkgd(win, COLOR_PAIR(color_index));
+}
+
 void update_char(int current, const char *code, WINDOW *win) {
   box(stdscr, 0, 0);
 
@@ -38,13 +79,38 @@ void update_char(int current, const char *code, WINDOW *win) {
   wrefresh(win);
 }
 
-int main(void) {
-  initscr();
+int main(int argc, char *argv[]) {
+  unsigned long current_char;
+  srand(time(NULL));
 
-  cbreak();
-  noecho();
+  if (argc > 1) {
+    char *code = read_file(argv[1]);
+    if (!code) {
+      fprintf(stderr, "[ERROR] Could not read file %s\n", argv[1]);
+      return 1;
+    }
 
-  setup_colors();
+    setup_curses();
+
+    WINDOW *win = create_window();
+    unsigned long code_len = strlen(code);
+    random_color(win);
+
+    while (1) {
+      getch();
+      current_char += TYPE_SPEED;
+      update_char(current_char, code, win);
+
+      if (current_char >= code_len) {
+        break;
+      }
+    }
+
+    return 0;
+  }
+
+  setup_curses();
+  WINDOW *win = create_window();
 
   const char *code[CODE_SNIPPETS] = {
       drw_c,           SvgLoadingContext_cpp, social_graph_snippets_py,
@@ -56,27 +122,14 @@ int main(void) {
     code_lengths[i] = strlen(code[i]);
   }
 
-  // get number of lines and cols
-  int lines, cols;
-  getmaxyx(stdscr, lines, cols);
-
-  WINDOW *win = newwin(lines - 2, cols - 2, 1, 1);
-  scrollok(win, TRUE);
-
-  srand(time(NULL));
-
-  unsigned long current_char;
   int snippet_index;
-  int color_index;
 
   while (1) {
-    // randomly select a snippet from the code
     current_char = 0;
-    snippet_index = rand() % CODE_SNIPPETS;
-    color_index = rand() % COLOR_COUNT + 1;
+    random_color(win);
 
-    wbkgd(stdscr, COLOR_PAIR(color_index));
-    wbkgd(win, COLOR_PAIR(color_index));
+    // randomly select a snippet from the code
+    snippet_index = rand() % CODE_SNIPPETS;
 
     while (1) {
       getch();
