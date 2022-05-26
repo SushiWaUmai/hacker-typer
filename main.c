8c98a5c262a3186616770f238e32614b28bddf20
SushiWaUmai <eugene.bb1@gmail.com>
2022-05-26 21:12:20 +0200

    feat: :sparkles: add different colors

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
+#include "code/SvgLoadingContext.h"
 #include "code/TimerManager.h"
 #include "code/commandHandler.h"
 #include "code/drw.h"
-#include "code/social_graph_snippets.h"
-#include "code/SvgLoadingContext.h"
 #include "code/helloworld.h"
+#include "code/social_graph_snippets.h"
 #include <curses.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define CODE_SNIPPETS 6
-#define TYPE_SPEED 5
+#define COLOR_COUNT 6
+#define TYPE_SPEED 10
 
 void setup_colors(void) {
   if (can_change_color()) {
     start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
-    wbkgd(stdscr, COLOR_PAIR(1));
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+    init_pair(4, COLOR_CYAN, COLOR_BLACK);
+    init_pair(5, COLOR_RED, COLOR_BLACK);
+    init_pair(6, COLOR_YELLOW, COLOR_BLACK);
     refresh();
   }
 }
@@ -40,11 +46,15 @@ int main(void) {
 
   setup_colors();
 
-  const char *code[CODE_SNIPPETS] = {drw_c, SvgLoadingContext_cpp,
-                                     social_graph_snippets_py, TimerManager_cs,
-                                     commandHandler_ts, helloworld_asm};
+  const char *code[CODE_SNIPPETS] = {
+      drw_c,           SvgLoadingContext_cpp, social_graph_snippets_py,
+      TimerManager_cs, commandHandler_ts,     helloworld_asm};
 
-  int current_char = 0;
+  unsigned long code_lengths[CODE_SNIPPETS];
+
+  for (int i = 0; i < CODE_SNIPPETS; i++) {
+    code_lengths[i] = strlen(code[i]);
+  }
 
   // get number of lines and cols
   int lines, cols;
@@ -52,20 +62,29 @@ int main(void) {
 
   WINDOW *win = newwin(lines - 2, cols - 2, 1, 1);
   scrollok(win, TRUE);
-  wbkgd(win, COLOR_PAIR(1));
 
   srand(time(NULL));
 
+  unsigned long current_char;
+  int snippet_index;
+  int color_index;
+
   while (1) {
     // randomly select a snippet from the code
-    int snippet_index = rand() % CODE_SNIPPETS;
+    current_char = 0;
+    snippet_index = rand() % CODE_SNIPPETS;
+    color_index = rand() % COLOR_COUNT + 1;
+
+    wbkgd(stdscr, COLOR_PAIR(color_index));
+    wbkgd(win, COLOR_PAIR(color_index));
 
     while (1) {
-      while (getch() != ERR) {
-        for (int i = 0; i < TYPE_SPEED; i++) {
-          current_char++;
-          update_char(current_char, code[snippet_index], win);
-        }
+      getch();
+      current_char += TYPE_SPEED;
+      update_char(current_char, code[snippet_index], win);
+
+      if (current_char >= code_lengths[snippet_index]) {
+        break;
       }
     }
   }
